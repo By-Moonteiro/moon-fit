3e6a413841e03a0765ea27f48418e34d0b987fbd
Moonteiro <owagnerlucas0@gmail.com>
2026-03-07 23:07:35 -0300

    feat: add bootstrap main function and api configs

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: '*',
+    AppModule: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('MoonFit')
+    .setDescription('Docs for MoonFit Project')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use('/docs', apiReference({ content: document, theme: 'deepSpace' }));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT')!;
+
+  await app.listen(port);
+}
+bootstrap().catch(console.error);
