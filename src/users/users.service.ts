1b99cb4de73067ec2452691953867ba92f14daa1
Moonteiro <owagnerlucas0@gmail.com>
2026-03-08 23:14:59 -0300

    fix: fix invalid return to findById method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -29,7 +29,7 @@ export class UsersService {
     const user = await this.userRepo.findById(userId);
 
     if (!user) {
-      return null;
+      throw new NotFoundException();
     }
 
     return user;
