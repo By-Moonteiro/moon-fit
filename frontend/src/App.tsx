b26e17a23c73cd61235a3992d0453d9b627ebb0c
Moonteiro <owagnerlucas0@gmail.com>
2026-03-26 21:10:04 -0300

    feat(frontend): implements onSubmit function and connect frontend with backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,23 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { MoonLogo } from "./components/MoonLogo"
 import { type LoginData, loginSchema } from "./schemas/login.schema"
+import { api } from "./lib/axios"
 
 export const App = () => {
 
   const  { register, handleSubmit, formState: { errors } }  = useForm<LoginData>({
     resolver: zodResolver(loginSchema)
   })
+
+  async function onSubmit(data: LoginData) {
+  try {
+    const response = await api.post('/auth/login', data)
+    localStorage.setItem('token', response.data.access_token)
+    console.log(localStorage.getItem('token'))
+  } catch (error) {
+    console.error('Erro ao fazer login:', error)
+  }
+}
 
   return (
     <div className='min-h-screen bg-slate-950 flex items-center justify-center'>
@@ -18,7 +29,7 @@ export const App = () => {
         <div>
           <p className='text-white text-center'>Entre na sua conta</p>
 
-          <form className='text-purple-300 flex flex-col gap-4' onSubmit={handleSubmit((data) => console.log(data))}> 
+          <form className='text-purple-300 flex flex-col gap-4' onSubmit={handleSubmit(onSubmit)}> 
             <label className='font-semibold  text-purple-300 text-sm mb-1 block' htmlFor="email">Email: </label>
             <input className='w-full bg-gray-800 text-white rounded-lg p-3 outline-none' type="email" placeholder='Digite seu email' id='email' { ...register('email')} />
             {errors.email && <span className='text-red-400 text-sm'>{errors.email.message}</span>}
