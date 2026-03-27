6f7edc8dcd845093cb1f68be04e138caf100c121
Moonteiro <owagnerlucas0@gmail.com>
2026-03-26 22:22:24 -0300

    feat(frontend): implements react-router to App file and set login and register routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LoginPage } from "./pages/Login"
+import { RegisterPage } from './pages/Register'
 
 
 export const App = () => {
-  return <LoginPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
