6d5bf60b12958be37330a22c5e6d604adbac7104
Moonteiro <owagnerlucas0@gmail.com>
2026-04-03 11:02:34 -0300

    feat(backend): adds refresh methods at auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,13 +4,42 @@ import { UsersService } from '../users/users.service';
 import { RegisterDto } from '../users/dtos/create-user.dto';
 import { LoginDto } from './dtos/login.dto';
 import { compare } from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
   ) {}
+
+  private generateTokens(userId: string, email: string) {
+    const payload = { sub: userId, email };
+
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_SECRET'),
+      expiresIn: '15m',
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      expiresIn: '7d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  async saveRefreshToken(userId: string, refreshToken: string) {
+    await this.usersService.updateRefreshToken(userId, refreshToken);
+  }
+
+  async refreshTokens(userId: string, email: string) {
+    const { accessToken, refreshToken } = this.generateTokens(userId, email);
+    await this.saveRefreshToken(userId, refreshToken);
+
+    return { accessToken, refreshToken };
+  }
 
   async register({ name, email, password }: RegisterDto) {
     return this.usersService.create({ name, email, password });
