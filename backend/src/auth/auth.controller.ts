170564b93658a7a4dae51f551f50c54d72168c7a
Moonteiro <owagnerlucas0@gmail.com>
2026-04-03 09:42:09 -0300

    refactor(backend): set access and refresh token to cookie on login endpoint

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
 import { AuthService } from './auth.service';
 import { RegisterDto } from '../users/dtos/create-user.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() user: LoginDto) {
-    return this.authService.login(user);
+  async login(@Body() user: LoginDto, @Res() res: Response) {
+    const { accessToken, refreshToken } = await this.authService.login(user);
+
+    res.cookie('access_token', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 15 * 60 * 1000, // 15 minutos em ms
+    });
+
+    res.cookie('refresh_token', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias em ms
+    });
+
+    return res.json({ message: 'Login successful' });
   }
 
   @Post('register')
